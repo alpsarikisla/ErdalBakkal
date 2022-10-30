e1c974abd34c69d2d79228a07d6612afcb382324
Alp Sarıkışla <50963484+alpsarikisla@users.noreply.github.com>
2022-10-30 14:59:43 +0300

    IlkCommit

---
+++ ErdalMarket/Depo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErdalMarket
+{
+    public class Depo
+    {
+        Urun[] urunler;
+
+        public Depo()
+        {
+            urunler = new Urun[0];
+        }
+
+        public void Ekle(Urun eleman)
+        {
+            Urun[] gecici = new Urun[urunler.Length + 1];
+            Array.Copy(urunler, gecici, urunler.Length);
+            gecici[gecici.Length - 1] = eleman;
+            urunler = gecici;
+        }
+
+        public void MenuYazdir()
+        {
+            for (int i = 0; i < urunler.Length; i++)
+            {
+               if (urunler[i].stok > 0)
+                {
+                    Console.WriteLine($"{i + 1} {urunler[i].isim}\t{urunler[i].fiyat} TL\t- Stok = {urunler[i].stok}");
+                }
+            }
+        }
+
+        public double FiyatGetir(int uruno)
+        {
+            double fiyat = urunler[uruno - 1].fiyat;
+            return fiyat;
+        }
+
+        public double SatinAl(int uruno, int adet)
+        {
+            double fiyat = 0;
+            if (urunler[uruno-1].stok >= adet)
+            {
+                fiyat = urunler[uruno - 1].fiyat;
+                urunler[uruno - 1].stok -= adet;
+            }
+            else
+            {
+                Console.WriteLine("Stok Yetersiz");
+            }
+            return fiyat;
+        }
+
+        public string BirimGetir(int uruno)
+        {
+            return urunler[uruno - 1].birim;
+        }
+    }
+}
---
+++ ErdalMarket/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErdalMarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Depo d = new Depo();
+            double toplam = 0;
+
+
+            #region Ürünleri Ekle
+
+            Urun u = new Urun();
+            u.isim = "Domates"; u.birim = "KG"; u.fiyat = 12.99; u.stok = 50;
+
+            d.Ekle(u);
+
+            Urun b = new Urun();
+            b.isim = "Bezelye"; b.birim = "KG"; b.fiyat = 20; b.stok = 125;
+
+            d.Ekle(b);
+
+            Urun c = new Urun(){isim = "Patlıcan",birim = "KG",fiyat = 25,stok = 120};
+            d.Ekle(c);
+
+            d.Ekle(new Urun() { isim = "Doritos", birim = "Paket", fiyat = 15, stok = 50 });
+
+            #endregion
+
+            Console.WriteLine("Erdal Market'e Hoş Geldiniz");
+            
+
+            string secenek = "e";
+            while (secenek == "e")
+            {
+                Console.WriteLine("Toplam = " + toplam + " TL");
+                d.MenuYazdir();
+                Console.WriteLine("Satın almak istediğiniz ürünün numarasını giriniz");
+                int no = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Kaç {d.BirimGetir(no)} alacaksınız?");
+                int adet = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(d.FiyatGetir(no) * adet + " TL");
+
+                toplam += d.SatinAl(no,adet) * adet;
+                Console.WriteLine("Alışverişe devam edilsin mi?e/h");
+                secenek = Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
